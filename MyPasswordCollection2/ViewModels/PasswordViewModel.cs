10c020a74e04782f5ea4fba929dfe572cecb6070
Alexandr <alexandr.psk@gmail.com>
2018-04-05 17:54:24 +0300

    1

---
+++ MyPasswordCollection2/ViewModels/PasswordViewModel.cs
@@ -0,0 +1,51 @@
+﻿using PasswordStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MPC.ViewModels
+{
+    class PasswordViewModel : BaseViewModel
+    {
+        private bool _editMode;
+        public bool EditMode
+        {
+            get { return _editMode; }
+            set
+            {
+                _editMode = value;
+                OnPropertyChanged(nameof(EditMode));
+            }
+        }
+
+        
+
+        private ICommand _copyCommand;
+
+        public ICommand CopyCommand
+        {
+            get {
+                return _copyCommand ??
+                  (_copyCommand = new Command<string>(CopyToClipboard)); }
+            set { _copyCommand = value; }
+        }
+
+
+
+        public PasswordItem PasswordItem { get; private set; }
+
+        public PasswordViewModel(PasswordItem passwordItem)
+        {
+            this.PasswordItem = passwordItem;
+        }
+
+        public void CopyToClipboard(string text)
+        {
+            Clipboard.SetText(text);
+        }
+    }
+}
---
+++ MyPasswordCollection2/Views/MultiBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MPC
+{
+    class MultiBoolConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+            foreach (var item in values)
+            {
+                result |= (bool)item;
+            }
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
