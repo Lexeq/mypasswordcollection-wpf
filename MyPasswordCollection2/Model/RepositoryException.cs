bf0983a6fffd747fddfaa68d86087101c6c05dda
Alexandr <alexandr.psk@gmail.com>
2019-01-15 00:16:43 +0300

    Add RepositoryException

---
+++ MyPasswordCollection2/Model/RepositoryException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MPC.Model
+{
+    public class RepositoryException: Exception
+    {
+        public RepositoryException() { }
+
+        public RepositoryException(string message) 
+            : base(message) { }
+
+        public RepositoryException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
