db35f3e66094cd75e280d835311156e730056711
Alex <alexandr.mail@abc.com>
2019-12-29 23:11:40 +0300

    Move focus to PasswordView when add new item

---
+++ MyPasswordCollection2/Views/FocusBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+
+namespace MPC.Views
+{
+    class FocusBehavior:Behavior<PasswordView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.GotFocus += AssociatedObject_GotFocus;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.GotFocus -= AssociatedObject_GotFocus;
+        }
+
+        private void AssociatedObject_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (e.OriginalSource == AssociatedObject)
+                AssociatedObject.tbSite.Focus();
+        }
+    }
+}
